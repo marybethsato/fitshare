aaa4db1ce1c463743072d4695c50453b648eba7d
mfosato <marybethsato@yahoo.com>
2024-12-10 22:48:52 -0700

    feat: logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+export default function Logo() {
+  return (
+    <div className="flex items-center">
+      <Image src="/assets/logo.png" alt="Logo" width={40} height={40} className="mr-2" />
+      <span className="text-2xl font-bold" style={{ fontFamily: 'Work Sans, sans-serif' }}>
+        FITSHARE
+      </span>
+    </div>
+  );
+}
